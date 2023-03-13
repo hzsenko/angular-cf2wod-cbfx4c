0637537392f79197a5e2c55b282adaea86015b96
hzsenko <hzsenko@mail.ru>
2023-03-13 16:09:53 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cf2wod-cbfx4c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cf2wod-cbfx4c)
---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+interface Account {
+  readonly name: string;
+  readonly account: string;
+}
+
+const BANK: readonly Account[] = [
+  {
+    name: 'Ruble',
+    account: '1234567890987654321',
+  },
+  {
+    name: 'Dollar',
+    account: '1234567890987654321',
+  },
+];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  value = null;
+  readonly bank = BANK;
+
+  onChange(value: readonly Account[]) {
+    this.value = !value.length ? null : value;
+  }
+}
