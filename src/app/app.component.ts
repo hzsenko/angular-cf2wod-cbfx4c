8326bf7e23ebcf5f1af04f9eda6e6acf3fc538f4
hzsenko <hzsenko@mail.ru>
2023-04-28 15:04:19 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,43 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-interface Account {
-  readonly name: string;
-  readonly account: string;
-}
-
-const BANK: readonly Account[] = [
-  {
-    name: 'Ruble',
-    account: '1234567890987654321',
-  },
-  {
-    name: 'Dollar',
-    account: '1234567890987654321',
-  },
-];
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: `my-app`,
+  templateUrl: `./app.component.html`,
+  styleUrls: [`./app.component.less`],
 })
 export class AppComponent {
-  value = null;
-  readonly bank = BANK;
+  readonly form = new FormGroup({
+    term: new FormControl(''),
+    additionalTerm: new FormControl(''),
+  });
+
+  filterBy = 'name';
+  additionalFilterBy = 'age';
+
+  readonly filter = (item: string, value: string): boolean => {
+    return `${item}`?.toLowerCase().includes(value?.toLowerCase());
+  };
+
+  readonly additionalFilter = (item: number, value: number): boolean => {
+    return !item ? true : item > value;
+  };
+
+  source = {
+    columns: ['name', 'age'],
+    data: [
+      {
+        name: 'john',
+        age: 20,
+      },
+      {
+        name: 'tom',
+        age: 25,
+      },
+      {
+        name: 'bob',
+        age: 19,
+      },
+    ],
+  };
 }
